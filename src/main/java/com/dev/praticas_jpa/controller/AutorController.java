f5800a625799ef3e4d567cfd03fa9f8504956fce
Tassochaves <tassogt@hotmail.com>
2024-11-13 23:25:23 -0300

    Feat: implementa atualização, exclusão, quantidade e busca de autores.

--- src/main/java/com/dev/praticas_jpa/controller/AutorController.java
+++ src/main/java/com/dev/praticas_jpa/controller/AutorController.java
@@ -1,9 +1,16 @@
 package com.dev.praticas_jpa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.praticas_jpa.dao.AutorDAO;
@@ -21,5 +28,39 @@ public class AutorController {
         autorDAO.salvar(autor);
 
         return autor;
+    }
+
+    @PutMapping
+    public Autor atualizar(@RequestBody Autor autor){
+        autorDAO.atualizar(autor);
+
+        return autor;
+    }
+
+    @DeleteMapping("{id}")
+    public String excluir(@PathVariable Long id){
+        autorDAO.excluir(id);
+
+        return "Autor id: " + id + " foi excluido com sucesso!";
+    }
+
+    @GetMapping("{id}")
+    public Autor retornaAutor(@PathVariable Long id){
+        return autorDAO.buscarPorId(id);
+    }
+
+    @GetMapping()
+    public List<Autor> retornaTodos(){
+        return autorDAO.buscarTodos();
+    }
+
+    @GetMapping("nome-ou-sobrenome")
+    public List<Autor> retornaTodosPorNomeOuSobrenome(@RequestParam String termo){
+        return autorDAO.buscarTodosPorNomeOuSobrenome(termo);
+    }
+
+    @GetMapping("total-de-autores")
+    public Long retornaTotalDeAutores(){
+        return autorDAO.buscarTotalDeAutor();
     }
 }
--- src/main/java/com/dev/praticas_jpa/dao/AutorDAO.java
+++ src/main/java/com/dev/praticas_jpa/dao/AutorDAO.java
@@ -1,5 +1,7 @@
 package com.dev.praticas_jpa.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,44 @@ public class AutorDAO {
     @Transactional(readOnly = false)
     public void salvar(Autor autor){
         this.manager.persist(autor);
+    }
+
+    @Transactional(readOnly = false)
+    public void atualizar(Autor autor){
+        this.manager.merge(autor);
+    }
+
+    @Transactional(readOnly = false)
+    public void excluir(Long id){
+        this.manager.remove(this.manager.getReference(Autor.class, id));
+    }
+
+    @Transactional(readOnly = true)
+    public Autor buscarPorId(Long id){
+        return this.manager.find(Autor.class, id);
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<Autor> buscarTodos(){
+        String query = "SELECT a FROM Autor a";
+        return this.manager.createQuery(query, Autor.class).getResultList();
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<Autor> buscarTodosPorNomeOuSobrenome(String termo){
+        String query = "SELECT a FROM Autor a WHERE a.nome LIKE :termo OR a.sobrenome LIKE :termo";
+        return this.manager.createQuery(query, Autor.class)
+        .setParameter("termo", "%" + termo + "%")
+        .getResultList();
+
+    }
+
+    @Transactional(readOnly = true)
+    public Long buscarTotalDeAutor(){
+        String query = "SELECT COUNT(1) FROM Autor a";
+        return this.manager.createQuery(query, Long.class).getSingleResult();
+
     }
 }
